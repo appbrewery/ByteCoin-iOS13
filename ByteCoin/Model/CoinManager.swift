f03907a157fbc5e1ec49771e22120c0c891ef3bd
TheMuellenator <TheMuellenator@users.noreply.github.com>
2020-02-03 16:44:19 +0000

    updating to coinapi.io

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct CoinManager {
     
-    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
     
 }
