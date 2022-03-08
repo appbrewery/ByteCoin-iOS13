ef1c6bd38f9326adbd9af8cb617bca4efd094fcc
m00racle <yanuar.prakosa@gmail.com>
2022-03-09 06:55:40 +0700

    making API request
    modify the CoinManager struct to make request, tested it and it came back OK with JSON data received. Next session we will decode these JSON data.

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,12 +11,39 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "0D68574D-46EE-4216-A6EB-806A274ABB83"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String){
         // get currency code and process to get data from the API.
-        print("passed currency : \(currency)")
+        // print("passed currency : \(currency)")
+        let url = baseURL + "/\(currency)" + "?apikey=\(apiKey)"
+        //test
+        print(url)
+        performRequest(urlString: url)
+    }
+    
+    func performRequest(urlString: String) {
+        // create URL
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            // test
+            print(dataString ?? "none")
+        }
     }
 }
