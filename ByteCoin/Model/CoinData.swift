725ea91d45e699856f79838174fad4dc47fe249a
m00racle <yanuar.prakosa@gmail.com>
2022-03-23 08:52:15 +0700

    add CoinData struct to decode JSON data
    This is the data type used to store values decoded from JSON data. The only thing declared here at this moment is the rate:Double which is the current (most actual) exchange rate of specified coin to currency.

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Yanuar Heru on 23/03/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+// this file we eill create a struct that represent decodable JSON data type
+
+struct CoinData: Codable {
+    let rate: Double
+}
